b41e5dbb9e6a2b6184578a9d5dc6a8d082fb7e82
Robert Schmidt <rob@c7a.ca>
2012-09-25 15:39:44 -0400

    inital clean module

---
+++ lib/Archive/BagIt.pm
@@ -0,0 +1,107 @@
+package Archive::BagIt;
+
+use 5.006;
+use strict;
+use warnings;
+
+=head1 NAME
+
+Archive::BagIt - The great new Archive::BagIt!
+
+=head1 VERSION
+
+Version 0.01
+
+=cut
+
+our $VERSION = '0.01';
+
+
+=head1 SYNOPSIS
+
+Quick summary of what the module does.
+
+Perhaps a little code snippet.
+
+    use Archive::BagIt;
+
+    my $foo = Archive::BagIt->new();
+    ...
+
+=head1 EXPORT
+
+A list of functions that can be exported.  You can delete this section
+if you don't export anything, such as for a purely object-oriented module.
+
+=head1 SUBROUTINES/METHODS
+
+=head2 function1
+
+=cut
+
+sub function1 {
+}
+
+=head2 function2
+
+=cut
+
+sub function2 {
+}
+
+=head1 AUTHOR
+
+Robert Schmidt, C<< <rjeschmi at gmail.com> >>
+
+=head1 BUGS
+
+Please report any bugs or feature requests to C<bug-archive-bagit at rt.cpan.org>, or through
+the web interface at L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=Archive-BagIt>.  I will be notified, and then you'll
+automatically be notified of progress on your bug as I make changes.
+
+
+
+
+=head1 SUPPORT
+
+You can find documentation for this module with the perldoc command.
+
+    perldoc Archive::BagIt
+
+
+You can also look for information at:
+
+=over 4
+
+=item * RT: CPAN's request tracker (report bugs here)
+
+L<http://rt.cpan.org/NoAuth/Bugs.html?Dist=Archive-BagIt>
+
+=item * AnnoCPAN: Annotated CPAN documentation
+
+L<http://annocpan.org/dist/Archive-BagIt>
+
+=item * CPAN Ratings
+
+L<http://cpanratings.perl.org/d/Archive-BagIt>
+
+=item * Search CPAN
+
+L<http://search.cpan.org/dist/Archive-BagIt/>
+
+=back
+
+
+=head1 ACKNOWLEDGEMENTS
+
+
+=head1 LICENSE AND COPYRIGHT
+
+Copyright 2012 Robert Schmidt.
+
+This program is released under the following license: cc0
+
+
+=cut
+
+1; # End of Archive::BagIt
